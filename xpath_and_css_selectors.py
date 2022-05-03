33135bf706e061cc3b29ce31e736547e624f425c
jeff502 <90343867+jeff502@users.noreply.github.com>
2022-05-03 08:08:03 -0500

    Create xpath_and_css_selectors.py

---
+++ xpath_and_css_selectors.py
@@ -0,0 +1,51 @@
+x_button = '//*[@id="root"]/div/div[1]/div/div/section/div/div[5]/div[2]' \
+           '/div[2]/div[1]/div/div/div[2]/div/div/div/div/div/div/div/div[1]/div/div[3]/button'
+channel_prediction_prompt = (
+    '//*[@id="channel-points-reward-center-body"]/div/div/div[1]/div/button'
+)
+
+predict_button_xpath = "/html/body/div[1]/div/div[1]/div/div/section/div/div[3]/div/div[2]" \
+                       "/div/div[3]/div/div/div[1]/div/div/div/div/div/div/div[1]/div/div/div/div/div[2]/button"
+
+predict_with_custom_amount = "/html/body/div[2]/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/section/" \
+                             "div/div[6]/div[2]/div[2]/div[1]/div/div/div[2]/div/div/div/div/div/div/div/div[2]" \
+                             "/div[3]/div/div/div/div/div/div/div/div/div[3]/div[2]/button"
+
+points_xpath = '//*[@id="root"]/div/div[1]/div/div/section/div/div[6]/div[2]/div[2]/div[1]/div/div/div/div[1]/div[2]' \
+               '/button/div/div/div/div[2]/span'
+
+blue_votes = '//*[@id="channel-points-reward-center-body"]/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div[3]' \
+             '/div[1]/div[1]/div/div/div[2]/p/span'
+
+red_votes = '//*[@id="channel-points-reward-center-body"]/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div[3]' \
+            '/div[1]/div[1]/div/div/div[2]/p/span'
+
+blue_field = '//*[@id="channel-points-reward-center-body"]/div/div/div/div/div/div/div[3]' \
+             '/div[1]/div/div/div[1]/div/div/div/input'
+
+red_field = "/html/body/div[1]/div/div[1]/div/div/section/div/div[6]/div[2]/div[2]/div[1]/div/div/div[2]/div/div/" \
+            "div/div/div/div/div/div[2]/div[3]/div/div/div/div/div/div/div/div/div[3]" \
+            "/div[1]/div/div/div[2]/div/div/div/input"
+
+blue_button = '//*[@id="channel-points-reward-center-body"]/div/div/div/div/div/div/div[3]/div[1]/div/div/div[1]' \
+              '/div/div/button'
+red_button = '//*[@id="channel-points-reward-center-body"]/div/div/div/div/div/div/div[3]/div[1]/div/div/div[2]' \
+             '/div/div/button'
+
+blue_end = '//*[@id="root"]/div/div[1]/div/div/section/div/div[6]/div[2]/div[2]/div[1]/div/div/div[2]/div/div' \
+           '/div/div/div/div/div/div[1]/div/div[3]/button'
+red_end = '//*[@id="root"]/div/div[1]/div/div/section/div/div[6]/div[2]/div[2]/div[1]/div/div/div[2]/div/div' \
+          '/div/div/div/div/div/div[1]/div/div[3]/button'
+
+timer = '//*[@id="channel-points-reward-center-body"]/div/div/div[1]/div/button/div/div[2]/p[2]'
+
+my_channel_points = ".fQvHcx"
+getting_started_prompt = (
+    '//*[@id="channel-points-reward-center-body"]/div/div/div[2]/button'
+)
+channel_points_reward_body = '//*[@id="channel-points-reward-center-body"]/div/div/div/div/div/div/' \
+                             'div[3]/div[2]/button/div/div'
+
+submission_text = ".dQTuLk"
+
+num_replace = {"K": 1000, "M": 1000000, "B": 1000000000}

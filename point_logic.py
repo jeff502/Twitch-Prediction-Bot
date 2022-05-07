9cd046c54939e6509833d85f6129403fd193cbc1
jeff502 <90343867+jeff502@users.noreply.github.com>
2022-05-06 19:30:35 -0500

    Update point_logic.py

--- point_logic.py
+++ point_logic.py
@@ -1,3 +1,5 @@
+import time
+
 num_replace = {
     "K": 1000,
     "M": 1000000,
@@ -6,15 +8,24 @@ num_replace = {
 
 
 def pure_number(string, number):
+    """
+    :param string: String with an abbreviations for a thousand, million, billion. ("k", "m", "b")
+    :param number: Int
+    :return: Returns the pure integer without abbreviations or commas for betting.
+    """
     mult = 1.0
     if string in num_replace:
         x = num_replace[string]
-        x = int(x)
         mult *= x
-        return number * mult
+        return int(number * mult)
 
 
 def get_points(current_points):
+    """
+    get_points and pure_number work in tandem to return an integer from our displayed channel points.
+    :param current_points:
+    :return: An integer
+    """
     if "K" in current_points:
         num = float(current_points.split("K")[0])
         character = current_points[len(current_points) - 1:]
@@ -27,6 +38,11 @@ def get_points(current_points):
 
 
 def how_much_to_bet(six_p, current_loop):
+    """
+    :param six_p: 6.25% of our total
+    :param current_loop: Current loop iteration
+    :return: 6.25% of our total * the current loop
+    """
     if current_loop == 1:
         return six_p
     elif current_loop == 2:
@@ -35,3 +51,25 @@ def how_much_to_bet(six_p, current_loop):
         return six_p * 4
     elif current_loop == 4:
         return six_p * 8
+
+
+def time_set(total_time):
+    """
+    Sleeps until time =< 1 minute
+    :param total_time: Total time remaining on an active prediction
+    :return: None
+    """
+    time_remaining = total_time.split(":")
+    if time_remaining[0] == "Prediction":
+        return
+    minutes = int(time_remaining[0])
+    seconds = int(time_remaining[1])
+
+    if minutes == 0:
+        return
+    elif minutes == 1:
+        time.sleep(seconds)
+        return
+    else:
+        time.sleep(((minutes - 1) * 60) + seconds)
+        return

42133f09d1b93dd0a67d138855fe90ad0ca3f391
jeff502 <90343867+jeff502@users.noreply.github.com>
2022-05-06 19:30:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import datetime
 import time
 from csv import DictReader
 from selenium import webdriver
@@ -6,7 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, \
     StaleElementReferenceException
-from point_logic import get_points, how_much_to_bet
+from point_logic import get_points, how_much_to_bet, time_set
+from prediction_logic import prediction_history, check_prediction_history
 from xpath_and_css_selectors import *
 
 
@@ -15,39 +15,6 @@ STREAMER = f"https://www.twitch.tv/popout/{CHANNEL_NAME}/chat"
 
 ser = Service("C:\Program Files (x86)\webdriver\chromedriver.exe")
 driver = webdriver.Chrome(service=ser)
-
-
-def time_set(total_time):
-    """
-    Sleeps until time =< 1 minute
-    :param total_time:
-    :return:
-    """
-    time_remaining = total_time.split(":")
-    if time_remaining[0] == "Prediction":
-        return
-    minutes = int(time_remaining[0])
-    seconds = int(time_remaining[1])
-
-    if minutes == 0:
-        return
-    elif minutes == 1:
-        time.sleep(seconds)
-        return
-    else:
-        time.sleep(((minutes - 1) * 60) + seconds)
-        return
-
-
-def prediction_history(color):
-    time_now = datetime.datetime.now().strftime('%H:%M:%S')
-    formatted_date = datetime.datetime.now().strftime('%b-%d-%y')
-    with open("Twitch Prediction History.txt", "a") as f:
-        f.write(
-            f"Streamer: {CHANNEL_NAME} | Points Bet: {points_to_bet} | Color: {color}|"
-            f" Loop Count: {current_loop_count} "
-            f"Time: {time_now} | Date: {formatted_date}\n"
-        )
 
 
 def get_cookie_values(file):
@@ -64,6 +31,9 @@ def get_cookie_values(file):
 
 
 def upload_cookies():
+    """
+    Reads and adds cookies to our browser. Refreshes the page when it's done.
+    """
     cookies = get_cookie_values("twitch_cookies.csv")
     for i in cookies:
         driver.add_cookie(i)
@@ -74,12 +44,14 @@ def upload_cookies():
 if __name__ == "__main__":
     driver.get(STREAMER)
     upload_cookies()
-    # time.sleep(120)
+    if check_prediction_history(CHANNEL_NAME):
+        total_before_bets, current_loop_count, six_percent = check_prediction_history(CHANNEL_NAME)
+    else:
+        six_percent = 1
+        current_loop_count = 1
+        total_before_bets = 0
 
     high_score = 0
-    six_percent = 1
-    current_loop_count = 1
-    total_before_bets = 0
     while True:
         try:
             time.sleep(2)
@@ -87,7 +59,6 @@ if __name__ == "__main__":
             channel_points.click()
             time.sleep(2)
 
-            # Getting started prompt happens during the first loop only.
             try:
                 driver.find_element(By.XPATH, getting_started_prompt).click()
             except NoSuchElementException as e:
@@ -99,8 +70,6 @@ if __name__ == "__main__":
             is_submission_closed = driver.find_element(
                 By.CSS_SELECTOR, submission_text
             ).text.lower()
-
-            # Checks if the submissions are closed or if voting has ended. Restarts the loop if True
 
             if "closed" in is_submission_closed:
                 try:
@@ -141,7 +110,8 @@ if __name__ == "__main__":
                 if current_loop_count > 4:
                     current_loop_count = 1
                     six_percent = (round(points * 0.0625))
-                    total_before_bets = 0
+                    total_before_bets = total_points
+
                 elif current_loop_count == 1:
                     six_percent = (round(points * 0.0625))
 
@@ -155,11 +125,10 @@ if __name__ == "__main__":
                 continue
 
             total_time_remaining = driver.find_element(By.XPATH, timer).text
-            total_time_for_func = total_time_remaining.split()[0]  # Getting the 0:00 time remaining
-            time_set(total_time_for_func)  # Sleeps until 1 minute remaining
+            total_time_for_func = total_time_remaining.split()[0]
+            time_set(total_time_for_func)
             time.sleep(2)
 
-            # Getting total votes for blue and red
             tbv = driver.find_element(By.XPATH, blue_votes).text
             trv = driver.find_element(By.XPATH, red_votes).text
 
@@ -183,8 +152,8 @@ if __name__ == "__main__":
                 red_vote_button.click()
                 time.sleep(2)
                 driver.find_element(By.XPATH, red_end).click()
-
-                prediction_history("Red")
+                prediction_history(CHANNEL_NAME, points_to_bet, "Red",
+                                   current_loop_count, total_before_bets, six_percent)
             else:
                 blue = driver.find_element(By.XPATH, blue_field)
                 blue.click()
@@ -194,9 +163,8 @@ if __name__ == "__main__":
                 blue_vote_button.click()
                 time.sleep(2)
                 driver.find_element(By.XPATH, blue_end).click()
-
-                prediction_history("Blue")
-
+                prediction_history(CHANNEL_NAME, points_to_bet, "Blue",
+                                   current_loop_count, total_before_bets, six_percent)
 
             current_loop_count += 1
         except (NoSuchElementException, ElementClickInterceptedException) as e:
